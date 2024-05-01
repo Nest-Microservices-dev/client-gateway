2deb24c27a6b07d412238a0546b6ffa3f7d7032d
cuter97 <aguscastets@gmail.com>
2024-05-01 18:47:27 -0300

    Add orders microservices

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -5,12 +5,18 @@ interface EnvVars {
     PORT: number;
     PRODUCTS_MICROSERVICES_HOST: string;
     PRODUCTS_MICROSERVICES_PORT: number;
+    ORDERS_MICROSERVICES_HOST: string;
+    ORDERS_MICROSERVICES_PORT: number;
 }
 
 const envsSchema = joi.object({
     PORT: joi.number().required(),
+
     PRODUCTS_MICROSERVICES_HOST: joi.string().required(),
     PRODUCTS_MICROSERVICES_PORT: joi.number().required(),
+    
+    ORDERS_MICROSERVICES_HOST: joi.string().required(),
+    ORDERS_MICROSERVICES_PORT: joi.number().required(),
 })
     .unknown(true);
 
@@ -26,6 +32,10 @@ const envVars: EnvVars = value;
 
 export const envs = {
     port: envVars.PORT,
+
     productsMicroservicesHost: envVars.PRODUCTS_MICROSERVICES_HOST,
     productsMicroservicesPort: envVars.PRODUCTS_MICROSERVICES_PORT,
+
+    ordersMicroservicesHost: envVars.ORDERS_MICROSERVICES_HOST,
+    ordersMicroservicesPort: envVars.ORDERS_MICROSERVICES_PORT,
 }
